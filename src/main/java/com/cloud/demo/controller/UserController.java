afa7162f2d08324ab2e085d38c0d7ffe2c11968f
Cloud <14128575+cloud_yyk@user.noreply.gitee.com>
2025-06-09 15:32:19 +0800

    在项目中引入swagger，添加注释

--- src/main/java/com/cloud/demo/controller/UserController.java
+++ src/main/java/com/cloud/demo/controller/UserController.java
@@ -4,6 +4,8 @@ package com.cloud.demo.controller;
 import com.cloud.demo.common.BaseResponse;
 import com.cloud.demo.common.ResponseUtil;
 import com.cloud.demo.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "用户模块", description = "用户相关接口")
 public class UserController {
 
     @Autowired
     private IUserService userService;
 
     @PutMapping("/register")
+    @Operation(summary = "用户注册", description = "传入用户名和密码")
     public BaseResponse<Long> userRegister(@RequestParam String username, @RequestParam String password, @RequestParam String checkedPassword) {
         userService.userRegister(username, password, checkedPassword);
         return ResponseUtil.success(null);
